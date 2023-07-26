478793a9d9006bca5e4379531291afcbeb444ef8
wannes-vlaanderen <138599016+wannes-vlaanderen@users.noreply.github.com>
2023-07-26 11:11:58 +0200

    Update main.js

--- examples/sidebar/main.js
+++ examples/sidebar/main.js
@@ -55,7 +55,7 @@ const map = new mapboxgl.Map({
 
 map.addControl(new LogoVlaanderen(), "top-left")
 map.addControl(new mapboxgl.AttributionControl({
-  customAttribution: "<a href="https://vlaanderen.be">Vlaamse Overheid</a>"
+  customAttribution: "<a href='https://vlaanderen.be'>Vlaamse Overheid</a>"
 }))
 
 function flyToLocation(currentFeature) {
